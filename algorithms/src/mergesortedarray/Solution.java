33fc9c741167361357967a5ef1f40aaa9495c943
yanshaoqiang <shaoqiang_yan@163.com>
2021-11-15 23:03:21 +0800

    88. 合并两个有序数组

---
+++ algorithms/src/mergesortedarray/Solution.java
@@ -0,0 +1,118 @@
+package mergesortedarray;
+
+import org.junit.Test;
+
+/**
+ * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+ *
+ * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * 输出：[1,2,2,3,5,6]
+ * 解释：需要合并 [1,2,3] 和 [2,5,6] 。
+ * 合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+ * 示例 2：
+ *
+ * 输入：nums1 = [1], m = 1, nums2 = [], n = 0
+ * 输出：[1]
+ * 解释：需要合并 [1] 和 [] 。
+ * 合并结果是 [1] 。
+ * 示例 3：
+ *
+ * 输入：nums1 = [0], m = 0, nums2 = [1], n = 1
+ * 输出：[1]
+ * 解释：需要合并的数组是 [] 和 [1] 。
+ * 合并结果是 [1] 。
+ * 注意，因为 m = 0 ，所以 nums1 中没有元素。nums1 中仅存的 0 仅仅是为了确保合并结果可以顺利存放到 nums1 中。
+ *
+ *
+ * 提示：
+ *
+ * nums1.length == m + n
+ * nums2.length == n
+ * 0 <= m, n <= 200
+ * 1 <= m + n <= 200
+ * -109 <= nums1[i], nums2[j] <= 109
+ *
+ *
+ * 进阶：你可以设计实现一个时间复杂度为 O(m + n) 的算法解决此问题吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author shaoqiangyan
+ */
+public class Solution {
+    /**
+     * 使用新的数组存储结果数组
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] result = new int[m+n];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < m || j < n) {
+            // 操作完数组之后再++
+//            k++;
+            // 防止越界
+            if (j >= n || (i<m && nums1[i] <= nums2[j])) {
+            // 什么时候要 nums[i]
+//            if (nums1[i] <= nums2[j]) {
+                result[k] = nums1[i];
+                i++;
+            } else {
+                result[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+        // result 里面的值放到 nums1中
+        for (int jj = 0;jj<result.length; jj++) {
+            nums1[jj] = result[jj];
+        }
+    }
+
+
+
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (i >= 0 || j >= 0) {
+            // 防止越界
+            if (j < 0 || (i >= 0 && nums1[i] >= nums2[j])) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+    }
+
+
+    @Test
+    public void test() {
+//        int[] nums1 = {1};
+//        int[] nums2 = {};
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        merge2(nums1, 3, nums2, 3);
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.print(nums1[i] + ",");
+        }
+    }
+}
+// i
+// 1,2,3
+// 2,5,6
+// j
